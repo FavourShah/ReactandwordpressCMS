5c8daceec2a464cc2305e93429533c64a84f2fbb
Favour Chiemewoya Ezechi-Oriaku <ezechioriakufavour@gmail.com>
2024-08-28 19:14:24 +0100

    made an API Proxy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/ReactandwordpressCMS/", // Use your repository name here
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://favourezechi.com.ng/wp/wp-json/wp/v2",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   build: {
     chunkSizeWarningLimit: 1000, // Set the limit to 1000 kB or any other value
   },
