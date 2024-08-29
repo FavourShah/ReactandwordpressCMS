119ba5b7c3b6bfd5ef2e2e571c5dee7b0fc7db00
Favour Chiemewoya Ezechi-Oriaku <ezechioriakufavour@gmail.com>
2024-08-29 15:28:35 +0100

    made chages in the homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Hero from "../Components/Hero";
 import PostListings from "../Components/PostListings";
 import HomeBody from "../Components/HomeBody";
@@ -5,7 +7,18 @@ import Contact from "../Components/Contact";
 import Testimonials from "../Components/Testimonials";
 
 const HomePage = () => {
-  const categoryId = 4; 
+  const categoryId = 4;
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
   return (
     <>
       <Hero />
